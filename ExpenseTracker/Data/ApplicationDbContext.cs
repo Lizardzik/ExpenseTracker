44190cbb1593f428f2a8c9cebeef544c8a0535ea
Lizardzik <rafaljasiura.business@gmail.com>
2024-05-31 14:32:39 +0200

    First Commit

---
+++ ExpenseTracker/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using ExpenseTracker.Models;
+using Microsoft.EntityFrameworkCore;
+using System.Data.Common;
+
+namespace ExpenseTracker.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Transaction> Transactions { get; set; }
+    }
+}

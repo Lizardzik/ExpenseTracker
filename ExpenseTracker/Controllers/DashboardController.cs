8e0c4a2fa8e4ad3db5c5dc614c49838243bcef1d
Lizardzik <rafaljasiura.business@gmail.com>
2024-06-03 18:08:55 +0200

    Sixth Commit

--- ExpenseTracker/Controllers/DashboardController.cs
+++ ExpenseTracker/Controllers/DashboardController.cs
@@ -17,10 +17,10 @@ namespace ExpenseTracker.Controllers
         public async Task<ActionResult> Index()
         {
             //Stats from a month
-            DateTime StartDate = DateTime.Today.AddDays(-30);
+            DateTime StartDate = DateTime.Today.AddDays(-7);
             DateTime EndDate = DateTime.Today;
 
-            List<Models.Transaction> transactions = await _context.Transactions.Include(x => x.Category).Where(y => y.Date >= StartDate && y.Date <= EndDate).ToListAsync();
+            List<Models.Transaction> transactions = await _context.Transactions.Include(x => x.Category).ToListAsync();
 
             //Income
             int Income = transactions.Where(i => i.Category.Type == "Income").Sum(j => j.Amount);
@@ -37,15 +37,51 @@ namespace ExpenseTracker.Controllers
             ViewBag.Balance = String.Format(culture, "{0:C0}", Balance);
 
             //Chars - Expenses
-            decimal totalExpenses = transactions.Where(i => i.Category.Type == "Expense").Sum(j => j.Amount);
             ViewBag.CharData = transactions.Where(i => i.Category.Type == "Expense").GroupBy(j => j.Category.CategoryId).Select(k => new
             {
                 categoryTitleWithIcon = k.First().Category.Icon+ " " + k.First().Category.Title,
                 amount = k.Sum(j => j.Amount),
                 formattedAmount = k.Sum(j => j.Amount).ToString("C0"),
+            }).OrderByDescending(l => l.amount).ToList();
+
+            //Chars - Income VS Expense
+
+            //Income
+            List<ChartData> incomeData = transactions.Where(i => i.Category.Type =="Income").GroupBy(j => j.Date).Select(k => new ChartData()
+            {
+                day = k.First().Date.ToString("dd-MMM"),
+                income = k.Sum(l => l.Amount)
             }).ToList();
+
+            //Expense
+            List<ChartData> expenseData = transactions.Where(i => i.Category.Type =="Expense").GroupBy(j => j.Date).Select(k => new ChartData()
+            {
+                day = k.First().Date.ToString("dd-MMM"),
+                expense = k.Sum(l => l.Amount)
+            }).ToList();
+
+            //Combine
+            string[] lastWeek = Enumerable.Range(0, 7).Select(i => StartDate.AddDays(i).ToString("dd-MMM")).ToArray();
+            ViewBag.ChartData = from day in lastWeek
+                                join income in incomeData on day equals income.day into dayIncome
+                                from income in dayIncome.DefaultIfEmpty()
+                                join expense in expenseData on day equals expense.day into dayExpense
+                                from expense in dayExpense.DefaultIfEmpty()
+                                select new
+                                {
+                                    day = day,
+                                    income = income == null ? 0 : income.income,
+                                    expense = expense == null ? 0 : expense.expense,
+                                };
 
             return View();
         }
+    }
+
+    public class ChartData
+    {
+        public string day;
+        public int income;
+        public int expense;
     }
 }
